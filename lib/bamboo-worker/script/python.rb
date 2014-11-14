dfd7f33dcad2c56ac3a15c544aedb4b68d7ed210
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2014-11-14 14:52:09 +0100

    Refactoring and add python script

--- lib/bamboo-worker/script/python.rb
+++ lib/bamboo-worker/script/python.rb
@@ -1,12 +1,51 @@
+# -*- coding: utf-8 -*-
+# Bamboo worker module
 module BambooWorker
-  module Script
-    # Default task for python language
-    class Python
+  # Script module
+  class Script
+    # Python language
+    class Python < Default
+      # Setup language with pyenv
+      #
+      def setup
+        super
+        export 'PYENV_VERSION', '$(pyenv versions ' \
+        "| grep ' #{@config.python}' " \
+        "| tail -1 | sed 's/[^0-9.]*\\([0-9.]*\\).*/\\1/'" \
+        "| sed -e 's/^[ \\t]*//')"
+        self.if('-z "$PYENV_VERSION"') do |klass|
+          klass.failure("#{@config.python} not found")
+        end
+      end
+
+      # Announce python, pyenv and bundle version
+      #
+      def announce
+        cmd 'python --version'
+        cmd 'pip --version'
+        cmd 'pyenv --version'
+      end
+
+      # Default install action for python
+      #
       def install
-        'pip install -r Requirements.txt' if File.exist?('Requirements.txt')
-        'pip install -r requirements.txt' if File.exist?('requirements.txt')
-        'Could not locate requirements.txt. ' \
-        'Override the install: key in your .travis.yml to install dependencies.'
+        self.if('-f Requirements.txt') do |klass|
+          klass.cmd 'pip install -r Requirements.txt', retry: true
+        end
+
+        elif '-f requirements.txt' do |klass|
+          klass.cmd 'pip install -r requirements.txt', retry: true
+        end
+
+        self.else do |klass|
+          klass.echo('Could not locate requirements.txt.')
+        end
+      end
+
+      # Default test script for python
+      #
+      def script
+        failure('No script found.')
       end
     end
   end
---
+++ spec/bamboo-worker/script/python_spec.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'travis/yaml'
+require 'bamboo-worker/shell'
+require 'bamboo-worker/script/default'
+require 'bamboo-worker/script/python'
+
+# Bamboo worker tests
+module BambooWorker
+  # python script tests
+  describe 'Script::Python' do
+    before(:each) do
+      travis = Travis::Yaml.matrix('language: python
+python: 3.3')
+      @python = Script::Python.new(travis.first)
+    end
+
+    it 'should setup' do
+      @python.setup
+      expect(@python.nodes.map(&:to_s))
+        .to eq(["export PYENV_VERSION=$(pyenv versions | grep ' 3.3' " \
+                "| tail -1 | sed 's/[^0-9.]*\\([0-9.]*\\).*/\\1/'| " \
+                "sed -e 's/^[ \\t]*//')",
+                "if [[ -z \"$PYENV_VERSION\" ]]; then\n  export " \
+                "BAMBOO_CMD=no_script\n  echo '3.3 not found'\nexit 1\nfi"])
+    end
+
+    it 'should announce' do
+      @python.announce
+      expect(@python.nodes.map(&:to_s))
+        .to eq(['python --version',
+                'pip --version',
+                'pyenv --version'])
+    end
+
+    it 'should install' do
+      @python.install
+      expect(@python.nodes.map(&:to_s))
+        .to eq(["if [[ -f Requirements.txt ]]; then\n  bamboo_cmd " \
+                "pip\\ install\\ -r\\ Requirements.txt --retry\nelif" \
+                " [[ -f requirements.txt ]]; then\n  bamboo_cmd " \
+                "pip\\ install\\ -r\\ requirements.txt --retry\nelse\n" \
+                "  echo 'Could not locate requirements.txt.'\nfi"])
+    end
+
+    it 'should script' do
+      @python.script
+      expect(@python.nodes.map(&:to_s))
+        .to eq(['export BAMBOO_CMD=no_script',
+                "echo 'No script found.'",
+                'exit 1'])
+    end
+  end
+end
