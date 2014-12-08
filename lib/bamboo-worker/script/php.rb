2210925f82c74fa793f7c1c65ceb8386814e5cac
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-12-08 17:45:24 +0100

    Support ndenv and phpenv

---
+++ lib/bamboo-worker/script/php.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Bamboo worker module
+module BambooWorker
+  # Script module
+  class Script
+    # Ruby language
+    class Php < Default
+      # Setup language with phpenv
+      #
+      def setup
+        super
+        # Phpenv is a fork of Rbenv, so it's normal to use the RBENV_VERSION
+        # variables -_-'
+        #
+        export 'RBENV_VERSION', "$(phpenv versions | grep #{@config.php} " \
+        "| tail -1 | sed 's/[^0-9.]*\\([0-9.]*\\).*/\\1/'" \
+        "| sed -e 's/^[ \\t]*//')"
+        self.if('-z "$RBENV_VERSION"') do |klass|
+          klass.failure("#{@config.php} not found")
+        end
+
+        export('BAMBOO_PHP_VERSION', @config.php, echo: false)
+      end
+
+      # Announce php and phpenv version
+      #
+      def announce
+        cmd 'php --version'
+        cmd 'phpenv --version'
+      end
+
+      # Default test script for php
+      #
+      def script
+        failure('No script found.')
+      end
+    end
+  end
+end
