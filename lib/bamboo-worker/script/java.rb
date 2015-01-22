23544e3afe96b424710ca2708ac279721dec67b2
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-22 18:30:46 +0100

    First commit for integrating java script

---
+++ lib/bamboo-worker/script/java.rb
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Bamboo worker module
+module BambooWorker
+  # Script module
+  class Script
+    # Java language
+    class Java < Default
+      # Setup language with jenv
+      #
+      def setup
+        super
+        export('JENV_VERSION', '$(jenv versions ' \
+               "| grep ' 1.#{@config.jdk} ' " \
+               "| tail -1 | sed 's/[^0-9.]*\\([0-9.]*\\).*/\\1/'" \
+               "| sed -e 's/^[ \\t]*//')")
+        self.if('-z "$JENV_VERSION"') do |klass|
+          klass.failure("Java #{@config.jdk} not found")
+        end
+
+        export('BAMBOO_JAVA_VERSION', @config.jdk, echo: false)
+      end
+
+      # Announce java and mvn version
+      #
+      def announce
+        cmd('jenv --version', echo: true)
+        cmd('java --version', echo: true)
+        cmd('javac --version', echo: true)
+        cmd('mvn --version', echo: true)
+      end
+
+      # Default install action for node js
+      #
+      def install
+        self.if('-f pom.xml') do |klass|
+          klass.cmd('mvn install -DskipTests=true' \
+                    ' -Dmaven.javadoc.skip=true -B -V', retry: true)
+        end
+      end
+
+      # Default test script for java
+      #
+      def script
+        self.if('-f pom.xml') do |klass|
+          klass.cmd('mvn test -B')
+        end
+        self.else do |klass|
+          klass.echo('Could not locate pom.xml file')
+        end
+      end
+    end
+  end
+end
---
+++ spec/bamboo-worker/script/java_spec.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+require 'travis/yaml'
+require 'bamboo-worker/shell'
+require 'bamboo-worker/script/default'
+require 'bamboo-worker/script/java'
+
+# Bamboo worker tests
+module BambooWorker
+  # java script tests
+  describe 'Script::Java' do
+    before(:each) do
+      travis = Travis::Yaml.matrix('language: java
+jdk: 6')
+      @java = Script::Java.new(travis.first)
+    end
+
+    it 'should setup' do
+      @java.setup
+      expect(@java.nodes.map(&:to_s))
+        .to eq(["export JENV_VERSION=$(jenv versions | grep ' 1.6 ' " \
+                "| tail -1 | sed 's/[^0-9.]*\\([0-9.]*\\).*/\\1/'| " \
+                "sed -e 's/^[ \\t]*//')",
+                "if [[ -z \"$JENV_VERSION\" ]]; then\n  export " \
+                "BAMBOO_CMD=no_script\n  echo 'Java 6 not found'\n  exit 1\nfi",
+                'export BAMBOO_JAVA_VERSION=6'])
+    end
+
+    it 'should announce' do
+      @java.announce
+      expect(@java.nodes.map(&:to_s))
+        .to eq(['bamboo_cmd jenv\\ --version --echo',
+                'bamboo_cmd java\\ --version --echo',
+                'bamboo_cmd javac\\ --version --echo',
+                'bamboo_cmd mvn\\ --version --echo'])
+    end
+
+    it 'should install' do
+      @java.install
+      expect(@java.nodes.map(&:to_s))
+        .to eq(["if [[ -f pom.xml ]]; then\n  " \
+                'bamboo_cmd mvn\\ install\\ -DskipTests\\=true\\' \
+                " -Dmaven.javadoc.skip\\=true\\ -B\\ -V --retry\nfi"])
+    end
+
+    it 'should script' do
+      @java.script
+      expect(@java.nodes.map(&:to_s))
+        .to eq(["if [[ -f pom.xml ]]; then\n  " \
+                "mvn test -B\nelse\n  " \
+                "echo 'Could not locate pom.xml file'\nfi"])
+    end
+  end
+end
